4884bb29177ff0068393c777174d12f17d78dc35
Dylan Huang <dylan.p.huang@gmail.com>
2018-10-23 15:36:48 -0500

    Removed some checks and rearranged Client.java

--- Client.java
+++ Client.java
@@ -8,6 +8,33 @@ import java.util.*;
 
 public class Client {
 
+    private static final int SERVER_PORT = 2017;
+    private static final String[] serverList = {
+            "fa18-cs425-g07-01.cs.illinois.edu",
+            "fa18-cs425-g07-02.cs.illinois.edu",
+            "fa18-cs425-g07-03.cs.illinois.edu",
+            "fa18-cs425-g07-04.cs.illinois.edu",
+            "fa18-cs425-g07-05.cs.illinois.edu",
+            "fa18-cs425-g07-06.cs.illinois.edu",
+            "fa18-cs425-g07-07.cs.illinois.edu",
+            "fa18-cs425-g07-08.cs.illinois.edu",
+            "fa18-cs425-g07-09.cs.illinois.edu",
+            "fa18-cs425-g07-10.cs.illinois.edu"
+    };
+    private static HashSet<String> commands = new HashSet<>(Arrays.asList(
+            "grep",
+            "exit",
+            "print",
+            "quit",
+            "log",
+            "put localfilename sdfsfilename",
+            "get sdfsfilename localfilename",
+            "ls sdsfilename",
+            "store",
+            "delete sdsfilename"
+    ));
+    private static String lastInput;
+
     public static String checkCommand(String cmd) {
         String ret = "INVALID";
         String[] components = cmd.split(" ");
@@ -26,34 +53,6 @@ public class Client {
         }
         return ret;
     }
-
-    private static HashSet<String> commands = new HashSet<>(Arrays.asList(
-            "grep",
-            "exit",
-            "print",
-            "quit",
-            "log",
-            "put localfilename sdfsfilename",
-            "get sdfsfilename localfilename",
-            "ls sdsfilename",
-            "store",
-            "delete sdsfilename"
-    ));
-
-    private static final int SERVER_PORT = 2017;
-    private static final String[] serverList = {
-            "fa18-cs425-g07-01.cs.illinois.edu",
-            "fa18-cs425-g07-02.cs.illinois.edu",
-            "fa18-cs425-g07-03.cs.illinois.edu",
-            "fa18-cs425-g07-04.cs.illinois.edu",
-            "fa18-cs425-g07-05.cs.illinois.edu",
-            "fa18-cs425-g07-06.cs.illinois.edu",
-            "fa18-cs425-g07-07.cs.illinois.edu",
-            "fa18-cs425-g07-08.cs.illinois.edu",
-            "fa18-cs425-g07-09.cs.illinois.edu",
-            "fa18-cs425-g07-10.cs.illinois.edu"
-    };
-    private static String lastInput;
 
     private static void execute(String cmd) throws IOException, InterruptedException {
         String[] cmds = cmd.split(" ");
@@ -123,15 +122,17 @@ class queryThread extends Thread implements Runnable {
 
     @Override
     public void run() {
-        String[] components = cmd.split(" ");
         try {
             socket = new Socket(ip, port);
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             writer = new DataOutputStream(socket.getOutputStream());
+
+            // Send command to server
             writer.writeBytes(cmd);
+
+            // Read response from server
             StringBuilder sb = new StringBuilder();
-            if (!components[0].equals("ls"))
-                sb.append(String.format("%s\n", ip));
+            sb.append(String.format("%s\n", ip));
             String line;
             while ((line = reader.readLine()) != null)
                 sb.append(String.format("%s", line));
@@ -140,11 +141,8 @@ class queryThread extends Thread implements Runnable {
                 if (!(sb.toString().trim().length() == 0))
                     System.out.println(sb.toString());
             }
-
         } catch (IOException e) {
-            if (!components[0].equals("ls"))
-                System.out.printf("Could not query to %s\n", ip);
-            return;
+            System.out.printf("Could not query to %s\n", ip);
         }
     }
 }
